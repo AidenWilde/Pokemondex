44dc5f0c6ba275f6f31ef95e9d18d78d2e2bfda4
Aiden <47725379+Mewrin@users.noreply.github.com>
2022-03-12 18:06:28 +0000

    Initial commit

---
+++ Api/PokemonApiClient.cs
@@ -0,0 +1,12 @@
+﻿namespace Pokemondex.Api
+{
+    public class PokemonApiClient
+    {
+        private HttpClient _httpClient;
+
+        public PokemonApiClient()
+        {
+            _httpClient = new HttpClient();
+        }
+    }
+}
---
+++ Api/PokemonApiWrapper.cs
@@ -0,0 +1,28 @@
+﻿namespace Pokemondex.Api
+{
+    public interface IPokemonApiWrapper
+    {
+        public void GetAll();
+        public void Get();
+    }
+
+    public class PokemonApiWrapper : IPokemonApiWrapper
+    {
+        private PokemonApiClient _client;
+
+        public PokemonApiWrapper()
+        {
+            _client = new PokemonApiClient();
+        }
+
+        public void GetAll()
+        {
+            // call off to client api to get all pokemon
+        }
+
+        public void Get()
+        {
+            // get specific pokemon information
+        }
+    }
+}
